53ca7deb9ec80d102eaf6dc6c7b62e86935e77dc
Mohamed Ali Youssef <43966369+mohamed-ali-youssef@users.noreply.github.com>
2023-08-12 23:19:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include "color.h"
+using namespace std;
+Color cr;
 
+int main()
+{
+    cout << cr.TextColor("Change the font color to red using [cr.FG.Red()]",
+    cr.FG.Red(),cr.BG.Null(),cr.B.False()) << "\n";
+
+    cout << cr.TextColor("Change the background color to red using [cr.BG.Red()]",
+    cr.FG.White(),cr.BG.Red(),cr.B.False()) << "\n";
+
+    cout << cr.TextColor("Change the background color to red using [cr.B.True()]",
+    cr.FG.White(),cr.BG.Null(),cr.B.True()) << "\n";
+
+    return 0;
+}
